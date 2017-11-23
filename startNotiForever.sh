94da8684b754027f180873919379b6061672236b
nadir93 <nadir93@gmail.com>
2017-11-23 14:33:30 +0900

    add somefiles

---
+++ startNotiForever.sh
@@ -0,0 +1,1 @@
+forever start ./notitome.js
---
+++ test.js
@@ -0,0 +1,8 @@
+const appium = require('appium');
+console.log('appium: '+appium);
+
+//let args = {port: 4723, host: '127.0.0.1'};
+//let server = new appium(args);
+
+//console.log('server: '+server);
+
