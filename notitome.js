76702fb888d27c16be85440f6d4ac4db76cef15b
parkty-at-755611797885 <nadir93@gmail.com>
2017-11-24 18:47:15 +0900

    refactoring

--- notitome.js
+++ notitome.js
@@ -4,20 +4,13 @@
  */
 'use strict';
 
-const Logger = require('bunyan');
-const loglevel = 'debug';
-const log = new Logger.createLogger({
-  name: 'notitome',
-  level: loglevel
-});
-
 const start = require('./lib/start');
 const interval = 3660000;
 
 //처음시작
 start()
   .then(() => {
-
+    console.log('completed');
   })
   .catch(log.error);
 
@@ -25,18 +18,18 @@ start()
 setInterval(() => {
   start()
     .then(() => {
-
+      console.log('completed');
     })
     .catch(log.error);
 }, interval);
 
 process.on('unhandledRejection', (reason, p) => {
-  log.debug('reason: ', reason);
-  log.debug('p: ', p);
+  console.log('reason: ', reason);
+  console.log('p: ', p);
   throw reason;
 });
 
 process.on('uncaughtException', e => {
-  log.debug('uncaughtException: ', e);
-  log.error(e);
+  console.log('uncaughtException: ', e);
+  console.error(e);
 });
